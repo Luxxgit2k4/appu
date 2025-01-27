1103e11865621117c303f1246bd41e07933084d2
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-01-27 22:53:53 +0530

    Added testing

---
+++ server.test.js
@@ -0,0 +1,12 @@
+import request from "supertest";
+import express from "express";
+const appu = express();
+appu.get("/", (req, res) => res.send("Welcome to Appu - Your Productivity Assistant!"));
+
+describe("Server Tests", () => {
+  it("should return the welcome message", async () => {
+    const response = await request(appu).get("/");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("Welcome to Appu - Your Productivity Assistant!");
+  });
+});
