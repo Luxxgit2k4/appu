8c8f54fb4aa590de3e2673863654b1150e1ed5b5
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-01-28 13:56:59 +0530

    Test cases for Eisenhower matrix

--- server.test.js
+++ server.test.js
@@ -21,4 +21,21 @@ test("Welcome message", async () => {
     expect(response.body.urgentImportant).toContain("Write tests")
   });
 
+test("Prioritizing tasks based on Eisenhower matrix", async () => {
+  const kumar1 = { name: "Kumar 1", urgent: true, important: true };
+const kumar2 = { name: "Kumar 2", urgent: false, important: true };
+const kumar3 = { name: "Kumar 3", urgent: true, important: false };
+const kumar4 = { name: "Kumar 4", urgent: false, important: false };
+
+  await request(appu).post("/tasks").send(kumar1);
+  await request(appu).post("/tasks").send(kumar2);
+await request(appu).post("/tasks").send(kumar3);
+await request(appu).post("/tasks").send(kumar4);
+
+  const response = await request(appu).get("/tasks");
+ expect(response.body.urgentImportant).toContain("Kumar 1");
+ expect(response.body.notUrgentImportant).toContain("Kumar 2");
+ expect(response.body.urgentNotImportant).toContain("Kumar 3");
+ expect(response.body.notUrgentNotImportant).toContain("Kumar 4");
+})
 });
